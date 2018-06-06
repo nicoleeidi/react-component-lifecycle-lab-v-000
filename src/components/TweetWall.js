746efbba0ffe70107944a99984a519864b8689bb
Nicole Eidi <nicoleeidi1@gmail.com>
2018-06-06 20:51:16 +0000

    automatically backed up by learn

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -18,8 +18,11 @@ class TweetWall extends React.Component {
       });
     }
   // TODO: shouldComponentUpdate()
+  
   // TODO: componentWillReceiveProps()
-
+componentWillReceiveProps(){
+  
+}
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
 
